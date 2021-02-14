0fa47f38315f07b13ed3220184c7b62cd783f1aa
William Dussault <dalloriam@gmail.com>
2021-02-14 15:38:08 -0500

    Document insert behavior in cache interface

--- crates/lfan/src/cache/cache.rs
+++ crates/lfan/src/cache/cache.rs
@@ -5,6 +5,10 @@ pub trait Cache {
     type Key: Hash + Eq + std::fmt::Debug;
     type Value;
 
+    /// Insert an item in the cache.
+    ///
+    /// The first return value indicates whether an insertion has taken place (because the cache can refuse to insert an item).
+    /// The second return value is the optional eviction victim, returned only if this call to insert caused an eviction.
     fn insert(&mut self, key: Self::Key, value: Self::Value) -> (bool, Option<Self::Value>);
 
     fn get<Q: ?Sized>(&mut self, key: &Q) -> Option<&Self::Value>
