552958ce9367d71c5e5b79c6d3464b95b58d992e
William Dussault <dalloriam@gmail.com>
2021-02-19 15:51:08 -0500

    Parallellize downloads

--- bin/menmos-cli/src/cli/download.rs
+++ bin/menmos-cli/src/cli/download.rs
@@ -19,6 +19,9 @@ pub struct DownloadCommand {
     /// The directory to which to save the files.
     #[clap(long = "out", short = 'o')]
     dst_dir: Option<PathBuf>,
+
+    #[clap(long = "concurrency", short = 'c', default_value = "4")]
+    concurrency: usize,
 }
 
 impl DownloadCommand {
@@ -28,44 +31,57 @@ impl DownloadCommand {
             let stdin = io::stdin();
             stdin.lock().lines().filter_map(|l| l.ok()).collect()
         } else {
-            self.blob_ids
+            self.blob_ids.clone()
         };
 
-        // TODO: Extract to service & parallelize.
         cli.step(format!("Downloading {} files...", &blob_ids.len()));
 
         let pushed = cli.push();
-        for blob_id in blob_ids.into_iter() {
-            pushed.step(format!("Downloading blob {}...", &blob_id));
-            let meta = match client.get_meta(&blob_id).await? {
-                Some(b) => b,
-                None => {
-                    cli.error("404 - Blob not found");
-                    continue;
-                }
-            };
+        let results = futures::stream::iter(blob_ids.into_iter())
+            .map(|blob_id| {
+                let pushed = pushed.clone();
+                let client = client.clone();
+                let cli = cli.clone();
+                let dst_dir = self.dst_dir.clone();
+                async move {
+                    let meta = match client.get_meta(&blob_id).await? {
+                        Some(b) => b,
+                        None => {
+                            cli.error("404 - Blob not found");
+                            return Ok(());
+                        }
+                    };
 
-            let stream = client.get_file(&blob_id).await?;
-            let mut stream_pin = Box::pin(stream);
+                    let stream = client.get_file(&blob_id).await?;
+                    let mut stream_pin = Box::pin(stream);
 
-            let file_path = match &self.dst_dir {
-                Some(d) => d.join(&meta.name),
-                None => PathBuf::from(meta.name),
-            };
+                    let file_path = match &dst_dir {
+                        Some(d) => d.join(&meta.name),
+                        None => PathBuf::from(meta.name),
+                    };
 
-            let mut f = fs::File::create(&file_path).await?;
+                    let mut f = fs::File::create(&file_path).await?;
 
-            while let Some(chunk) = stream_pin.next().await {
-                match chunk {
-                    Ok(c) => f.write_all(c.as_ref()).await?,
-                    Err(e) => {
-                        fs::remove_file(&file_path).await?;
-                        return Err(anyhow!("{}", e.to_string()));
+                    while let Some(chunk) = stream_pin.next().await {
+                        match chunk {
+                            Ok(c) => f.write_all(c.as_ref()).await?,
+                            Err(e) => {
+                                fs::remove_file(&file_path).await?;
+                                return Err(anyhow!("{}", e.to_string()));
+                            }
+                        }
                     }
+                    pushed.success(format!("Downloaded {} to {:?}", blob_id, file_path));
+
+                    Ok(())
                 }
-            }
-            pushed.success("Done");
-        }
+            })
+            .buffer_unordered(self.concurrency)
+            .collect::<Vec<Result<()>>>()
+            .await;
+
+        // Catch any errors that occurred.
+        results.into_iter().collect::<Result<Vec<()>>>()?;
 
         cli.success("Done.");
         Ok(())
