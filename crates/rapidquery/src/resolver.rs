880bb522942f30caeb5c343523a9f96fff1a0d39
William Dussault <dalloriam@gmail.com>
2021-02-21 23:42:41 -0500

    Properly test & document rapidquery

--- crates/rapidquery/src/resolver.rs
+++ crates/rapidquery/src/resolver.rs
@@ -1,14 +1,53 @@
 use std::ops;
 
+/// Trait describing a query context resolver.
+///
+/// The resolver is needed by RapidQuery to provide the initial values for computing the query.
+///
+/// For instance, when evaluating the query `(a || b) && c`, the evaluator will call `Resolver::load_tag()`
+/// with `a`, `b`, and `c`. From there, it'll be in a position to compute the final result of the query.
 pub trait Resolver<V>
 where
     V: ops::BitAndAssign + ops::BitOrAssign + ops::Not,
 {
+    /// The error type returned by the resolver.
     type Error;
 
+    /// Resolves the set matching a given tag.
     fn resolve_tag(&self, tag: &str) -> Result<V, Self::Error>;
+
+    /// Resolves the set matching a given key/value pair.
     fn resolve_key_value(&self, key: &str, value: &str) -> Result<V, Self::Error>;
+
+    /// Resolves the set matching any key/value pairs with the given key.
     fn resolve_key(&self, key: &str) -> Result<V, Self::Error>;
+
+    /// Resolves the set matching a given parent ID.
     fn resolve_children(&self, parent_id: &str) -> Result<V, Self::Error>;
+
+    /// Resolves the set of all possible items that could match a query.
     fn resolve_empty(&self) -> Result<V, Self::Error>;
+}
+
+impl<V, E> Resolver<V> for Box<dyn Resolver<V, Error = E>>
+where
+    V: ops::BitAndAssign + ops::BitOrAssign + ops::Not,
+{
+    type Error = E;
+
+    fn resolve_tag(&self, tag: &str) -> Result<V, Self::Error> {
+        (**self).resolve_tag(tag)
+    }
+    fn resolve_key_value(&self, key: &str, value: &str) -> Result<V, Self::Error> {
+        (**self).resolve_key_value(key, value)
+    }
+    fn resolve_key(&self, key: &str) -> Result<V, Self::Error> {
+        (**self).resolve_key(key)
+    }
+    fn resolve_children(&self, parent_id: &str) -> Result<V, Self::Error> {
+        (**self).resolve_children(parent_id)
+    }
+    fn resolve_empty(&self) -> Result<V, Self::Error> {
+        (**self).resolve_empty()
+    }
 }
--- crates/rapidquery/src/span.rs
+++ crates/rapidquery/src/span.rs
@@ -1,6 +1,15 @@
+/// Trait used for computing the precedence order of operations.
+///
+/// Sets with a higher `Span` value will be prioritized during evaluation.
 pub trait Span {
     /// Returns the span of values covered by this type.
     fn span(&self) -> usize;
+}
+
+impl Span for bool {
+    fn span(&self) -> usize {
+        1
+    }
 }
 
 #[cfg(feature = "bitvec_span")]
