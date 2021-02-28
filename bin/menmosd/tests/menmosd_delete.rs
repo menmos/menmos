8731a3bac580ffb610e67d2b57ee4001ea87d93e
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-02-28 15:12:55 -0500

    tests

---
+++ bin/menmosd/tests/menmosd_delete.rs
@@ -0,0 +1,25 @@
+//! Test blob deletion capabilities.
+mod util;
+
+use anyhow::Result;
+use menmos_client::Meta;
+use testing::fixtures::Menmos;
+
+#[tokio::test]
+async fn delete_blob() -> Result<()> {
+    let mut cluster = Menmos::new().await?;
+    cluster.add_amphora("alpha").await?;
+
+    let blob_id = cluster
+        .push_document("Hello world!", Meta::file("test_blob"))
+        .await?;
+
+    cluster.client.delete(blob_id.clone()).await?;
+
+    let file = cluster.client.get_file(&blob_id).await;
+    assert!(file.is_err());
+
+    cluster.stop_all().await?;
+
+    Ok(())
+}
