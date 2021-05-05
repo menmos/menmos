882f2eb7fd8b50269aa4ea0292324d036217f40f
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-05-04 22:29:32 -0400

    add support for cors in debug

--- bin/menmosd/src/menmosd/server/filters/mod.rs
+++ bin/menmosd/src/menmosd/server/filters/mod.rs
@@ -13,6 +13,7 @@ use warp::Filter;
 
 use crate::server::Context;
 
+#[cfg(not(debug))]
 pub fn all(
     context: Context,
 ) -> impl Filter<Extract = impl warp::Reply, Error = Infallible> + Clone {
@@ -25,4 +26,27 @@ pub fn all(
         .or(routing::all(context))
         .recover(apikit::reject::recover)
         .with(warp::log("directory::api"))
+}
+
+#[cfg(debug)]
+pub fn all(
+    context: Context,
+) -> impl Filter<Extract = impl warp::Reply, Error = Infallible> + Clone {
+    storage::all(context.clone())
+        .or(blob::all(context.clone()))
+        .or(blobmeta::all(context.clone()))
+        .or(admin::all(context.clone()))
+        .or(query::all(context.clone()))
+        .or(auth::all(context.clone()))
+        .or(routing::all(context))
+        .with(
+            warp::cors()
+                .allow_any_origin()
+                .allow_headers(vec!["Content-Type", "x-blob-meta"])
+                .allow_methods(vec!["GET", "POST", "DELETE", "PUT", "OPTIONS"]),
+        )
+        .with(warp::log("directory::api"))
+        .recover(apikit::reject::recover);
+
+    filters
 }
