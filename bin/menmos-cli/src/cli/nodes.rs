b0247702f54ca3ec212414b7e00d6081e33cf493
William Dussault <dalloriam@gmail.com>
2021-12-29 18:43:26 -0500

    Add CLI call to list nodes from CLI

---
+++ bin/menmos-cli/src/cli/nodes.rs
@@ -0,0 +1,49 @@
+use anyhow::Result;
+use clap::Parser;
+use menmos_client::Client;
+use rood::cli::OutputManager;
+
+#[derive(Parser)]
+pub struct ListStorageNodesCommand {}
+
+impl ListStorageNodesCommand {
+    pub async fn run(self, cli: OutputManager, client: Client) -> Result<()> {
+        cli.step("Storage Nodes");
+
+        let storage_nodes = client.list_storage_nodes().await?;
+
+        let pushed = cli.push();
+
+        for node in storage_nodes.storage_nodes {
+            pushed.step(node.id);
+
+            let pushed = pushed.push();
+            pushed.step(format!("Port: {}", node.port));
+            pushed.step(format!(
+                "Storage Used: {:.2}/{} Gb",
+                node.size as f32 / (1024.0 * 1024.0 * 1024.0),
+                (node.available_space + node.size) / (1024 * 1024 * 1024)
+            ));
+            pushed.step("Redirect Info");
+            let pushed = pushed.push();
+
+            match node.redirect_info {
+                interface::RedirectInfo::Automatic {
+                    public_address,
+                    local_address,
+                    subnet_mask,
+                } => {
+                    pushed.step(format!("Public Address: {}", public_address));
+                    pushed.step(format!("Local Address: {}", local_address));
+                    pushed.step(format!("Subnet Mask: {}", subnet_mask));
+                }
+
+                interface::RedirectInfo::Static { static_address } => {
+                    pushed.step(format!("Static Address: {}", static_address));
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
