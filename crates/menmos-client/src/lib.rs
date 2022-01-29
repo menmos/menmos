03c9b41cf3f09c8d85e2f667859314193d7eb819
William Dussault <dalloriam@gmail.com>
2022-01-29 14:01:50 -0500

    Actually remove metadata detector

--- crates/menmos-client/src/lib.rs
+++ crates/menmos-client/src/lib.rs
@@ -1,6 +1,5 @@
 mod builder;
 mod client;
-mod metadata_detector;
 mod parameters;
 
 use builder::ClientBuilder;
@@ -10,6 +9,3 @@ pub use client::Client;
 pub use interface::BlobMetaRequest as Meta;
 pub use interface::Type;
 pub use interface::{Expression, Query, QueryResponse};
-
-#[cfg(test)]
-mod test;
--- crates/menmos-client/src/test/mod.rs
+++
@@ -1,1 +0,0 @@
-mod metadata_detector;
