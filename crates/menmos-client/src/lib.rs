536d267af45c5ff97af1b7cbd515559e1676ddb2
William Dussault <dalloriam@gmail.com>
2022-01-30 00:23:51 -0500

    Expose error type

--- crates/menmos-client/src/lib.rs
+++ crates/menmos-client/src/lib.rs
@@ -5,7 +5,7 @@ mod parameters;
 use builder::ClientBuilder;
 use parameters::Parameters;
 
-pub use client::Client;
+pub use client::{Client, ClientError};
 pub use interface::BlobMetaRequest as Meta;
 pub use interface::Type;
 pub use interface::{Expression, Query, QueryResponse};
