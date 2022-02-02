34c048aa61161e7dbe7c988f8527f6d8cb3f2e46
William Dussault <dalloriam@gmail.com>
2022-02-01 23:44:15 -0500

    Add an endpoint to get the effective node configuration

--- bin/menmosd/src/menmosd/server/handlers/admin/mod.rs
+++ bin/menmosd/src/menmosd/server/handlers/admin/mod.rs
@@ -1,10 +1,12 @@
 mod flush;
+mod get_config;
 mod health;
 mod rebuild;
 mod rebuild_complete;
 mod version;
 
 pub use flush::flush;
+pub use get_config::get_config;
 pub use health::health;
 pub use rebuild::rebuild;
 pub use rebuild_complete::rebuild_complete;
