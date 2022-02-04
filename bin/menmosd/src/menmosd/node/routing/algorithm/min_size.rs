1389eb03b9eabe534a148f488d11cff8f5623ed7
William Dussault <dalloriam@gmail.com>
2022-02-04 18:14:07 -0500

    Properly test the Min-Size policy (#186)

--- bin/menmosd/src/menmosd/node/routing/algorithm/min_size.rs
+++ bin/menmosd/src/menmosd/node/routing/algorithm/min_size.rs
@@ -52,3 +52,88 @@ impl RoutingPolicy for MinSizePolicy {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::net::IpAddr;
+
+    use anyhow::Result;
+    use interface::RedirectInfo;
+
+    use super::*;
+
+    fn get_storage_node(name: &str, available_space: u64) -> StorageNodeInfo {
+        StorageNodeInfo {
+            id: String::from(name),
+            redirect_info: RedirectInfo::Static {
+                static_address: IpAddr::from([127, 0, 0, 1]),
+            },
+            port: 4242,
+            size: 0,
+            available_space,
+        }
+    }
+
+    #[tokio::test]
+    async fn policy_selects_node_with_most_free_space() -> Result<()> {
+        let policy = MinSizePolicy::default();
+
+        policy.add_node(get_storage_node("a", 200)).await;
+        policy.add_node(get_storage_node("b", 50)).await;
+
+        let candidate = policy.get_candidate().await.unwrap();
+
+        assert_eq!(&candidate, "a");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn node_updates_changes_selected_candidate() -> Result<()> {
+        let policy = MinSizePolicy::default();
+
+        let node_a = get_storage_node("a", 200);
+        let mut node_b = get_storage_node("b", 300);
+
+        policy.add_node(node_a.clone()).await;
+        policy.add_node(node_b.clone()).await;
+
+        // validate the first candidate is node b.
+        let candidate = policy.get_candidate().await.unwrap();
+        assert_eq!(&candidate, "b");
+
+        node_b.available_space = 100;
+        policy.update_node(node_b).await;
+
+        // validate the first candidate is now node a.
+        let candidate = policy.get_candidate().await.unwrap();
+        assert_eq!(&candidate, "a");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn prune_last_removes_best_candidate() -> Result<()> {
+        let policy = MinSizePolicy::default();
+
+        policy.add_node(get_storage_node("a", 200)).await;
+        policy.add_node(get_storage_node("b", 100)).await;
+
+        let candidate = policy.get_candidate().await.unwrap();
+        assert_eq!(&candidate, "a");
+
+        assert_eq!(policy.prune_last().await, Some(String::from("a")));
+
+        // make sure "a" isn't in anymore
+        let candidate = policy.get_candidate().await.unwrap();
+        assert_eq!(&candidate, "b");
+
+        // pruning again should remove b.
+        assert_eq!(policy.prune_last().await, Some(String::from("b")));
+
+        // pruning again should return none
+        assert_eq!(policy.prune_last().await, None);
+
+        Ok(())
+    }
+}
